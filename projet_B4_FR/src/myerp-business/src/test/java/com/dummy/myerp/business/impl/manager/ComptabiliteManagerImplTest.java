18a0207a9578f1961431b9b6c80d80b0b3c79831
tomneozed <tomneozed@gmail.com>
2019-09-18 19:47:55 +0200

    added mockito dependency & ComptabiliteManagerImplTest updated

--- projet_B4_FR/src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
+++ projet_B4_FR/src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
@@ -11,7 +14,7 @@ import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.SequenceEcritureComptable;
 import com.dummy.myerp.technical.exception.FunctionalException;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class ComptabiliteManagerImplTest {
 
     private ComptabiliteManagerImpl manager = new ComptabiliteManagerImpl();
@@ -33,6 +36,10 @@ public class ComptabiliteManagerImplTest {
         manager.checkEcritureComptableUnit(vEcritureComptable);
     }
 
+    /**
+     * Vérification du respect des règles de gestion unitaires de l'écriture comptable
+     * @throws Exception
+     */
     @Test(expected = FunctionalException.class)
     public void checkEcritureComptableUnitViolation() throws Exception {
         EcritureComptable vEcritureComptable;
@@ -40,6 +47,10 @@ public class ComptabiliteManagerImplTest {
         manager.checkEcritureComptableUnit(vEcritureComptable);
     }
 
+    /********************************************* RG2 **********************************************
+     * Vérification qu'une écriture comptable non équilibrée renvoie une exception
+     * @throws Exception
+     */
     @Test(expected = FunctionalException.class)
     public void checkEcritureComptableUnitRG2() throws Exception {
         EcritureComptable vEcritureComptable;
@@ -56,6 +67,10 @@ public class ComptabiliteManagerImplTest {
         manager.checkEcritureComptableUnit(vEcritureComptable);
     }
 
+    /********************************************* RG3 **********************************************
+     * Vérification qu'une écriture comptable contient au moins 2 lignes d'écriture (débit & crédit)
+     * @throws Exception
+     */
     @Test(expected = FunctionalException.class)
     public void checkEcritureComptableUnitRG3() throws Exception {
         EcritureComptable vEcritureComptable;
@@ -72,6 +87,18 @@ public class ComptabiliteManagerImplTest {
         manager.checkEcritureComptableUnit(vEcritureComptable);
     }
     
+    /********************************************* RG4 **********************************************
+     * Vérification qu'une écriture comptable peut contenir une ligne avec montant négatif
+     */
+    
+    
+    
+    
+    
+    /********************************************* RG5 (date) **********************************************
+     * Vérification de la cohérence de la référence de l'écriture comptable avec l'année d'écriture 
+     * @throws Exception
+     */
     @Test()
     public void checkEcritureComptableUnitRG5() throws Exception {
     	
@@ -79,13 +106,20 @@ public class ComptabiliteManagerImplTest {
     	vSequenceEcritureComptable = new SequenceEcritureComptable();
     	vSequenceEcritureComptable.setAnnee(2018);
     	vSequenceEcritureComptable.setDerniereValeur(00001);
-    	
-    	
     }
     
+    /********************************************* RG5 (journal) **********************************************
+     * Vérification de la cohérence de la référence de l'écriture comptable avec le code du journal
+     * @throws Exception
+     */
     
+    /********************************************* RG6 **********************************************
+     * Vérification que la référence des écritures comptables est unique
+     * @throws Exception
+     */
     
-    
-    
-
+    /********************************************* RG7 **********************************************
+     * Vérification que les montants des lignes d'écriture sont arrondis au centième 
+     * @throws Exception
+     */
 }
