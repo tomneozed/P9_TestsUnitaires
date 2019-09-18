c92976eb4e62d92aed0258862fae9506885008f9
tomneozed <tomneozed@gmail.com>
2019-09-18 20:36:01 +0200

    Jenkins tests

--- projet_B4_FR/src/myerp-business/src/main/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImpl.java
+++ projet_B4_FR/src/myerp-business/src/main/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImpl.java
@@ -232,6 +232,7 @@ public class ComptabiliteManagerImpl extends AbstractBusinessManager implements 
         }
     }
 
+    
     /**
      * Vérifie que l'Ecriture comptable respecte les règles de gestion liées au contexte
      * (unicité de la référence, année comptable non cloturé...)
